5f032a96d3211dc4f7a8230936e751c0fd5d86e7
Asil_Abdihamidov <165748357+Cid20@users.noreply.github.com>
2024-10-11 15:25:07 +0500

    Add files via upload

---
+++ shedu_1/src/main/java/sds_java/shedu_1/Shedu1Application.java
@@ -0,0 +1,13 @@
+package sds_java.shedu_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shedu1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shedu1Application.class, args);
+    }
+
+}
---
+++ shedu_1/src/test/java/sds_java/shedu_1/Shedu1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sds_java.shedu_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shedu1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
